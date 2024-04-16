3ef4a44d738ecfa6645d6838c78c2c90c59fdcf7
Dixit <mail@dixit.dev>
2024-04-16 22:44:55 +0530

    initial config

---
+++ after/plugin/fugitive.lua
@@ -0,0 +1,2 @@
+print(".config/nvim/after/plugin/fugitive.lua")
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
---
+++ init.lua
@@ -0,0 +1,86 @@
+print("Hello from .config/nvim/init.lua")
+
+vim.opt.guicursor = ""
+vim.opt.wrap = false
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.termguicolors = true
+vim.opt.updatetime = 50
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.colorcolumn = "80"
+vim.opt.isfname:append("@-@")
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+---- Visual Mode Line Up and Down Moving ----
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+---- Cursor Stay in place when you applying lines ----
+vim.keymap.set("n", "J", "mzJ`z")
+
+---- Center Cursor when Half Page Jumping ----
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+---- Allows Searches to Stay in Center ----
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+---- Copy remap ----
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- This is going to get me cancelled
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+---- Replace ----
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+---- Add Executable Permission to Current File ----
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+
+
+---- PACKER PLUGIN MANAGER ----
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+
+    use({ 'rose-pine/neovim', as = 'rose-pine'})
+    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use('mbbill/undotree')
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.6',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    use('theprimeagen/harpoon')
+    use('tpope/vim-fugitive')
+
+
+end)
+
