8cf4b48292bbbc0bffc0d04840c769fdd26ae016
Dixit Bhuva <mail@dixit.dev>
2025-01-08 18:42:01 +0530

    forget to add leader key

--- init.lua
+++ init.lua
@@ -2,6 +2,10 @@ vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_python3_provider = 0
 
+----- LEADER KEY -----
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 ----- BASIC OPTIONS -----
 vim.opt.updatetime = 50
 vim.opt.guicursor = ""
