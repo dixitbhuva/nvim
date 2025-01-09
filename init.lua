51c0b10382b038fe526fa3ecbe2726bceb33d340
Dixit Bhuva <mail@dixit.dev>
2025-01-09 10:01:50 +0530

    Forget to add leader key

--- init.lua
+++ init.lua
@@ -2,6 +2,8 @@ vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_python3_provider = 0
 
+vim.cmd [[colorscheme retrobox]]
+
 ----- LEADER KEY -----
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
