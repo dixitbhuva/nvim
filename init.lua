1193b2f16f4b3814f143d609c58be10751647103
Dixit Bhuva <mail@dixit.dev>
2025-02-21 12:17:08 +0530

    last used

--- init.lua
+++ init.lua
@@ -65,10 +65,11 @@ vim.opt.splitbelow = true
 vim.keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 vim.keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
 vim.keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
-vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
-vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
-vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
-vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
+-- commented out because this keys overlaps with telescope keymaps
+--vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+--vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+--vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
+--vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
 vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
 vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
 vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
@@ -84,13 +85,51 @@ require("lazy").setup({
   spec = {
     -- add your plugins here
     --------------------------------------------------------------------------------
-    { 
-      "ellisonleao/gruvbox.nvim",
-      lazy = false,
-      priority = 1000,
+    --  { 
+    --    "folke/tokyonight.nvim",
+    --    lazy = false,
+    --    priority = 1000,
+    --    config = function()
+    --      vim.opt.background = "dark"
+    --      vim.cmd([[colorscheme tokyonight-night]])
+    --    end,
+    --  },
+    {
+      "folke/tokyonight.nvim",
+      priority = 1000, -- make sure to load this before all other start plugins
       config = function()
-        vim.opt.background = "dark"
-        vim.cmd([[colorscheme gruvbox]])
+        local bg = "#011628"
+        local bg_dark = "#011423"
+        local bg_highlight = "#143652"
+        local bg_search = "#0A64AC"
+        local bg_visual = "#275378"
+        local fg = "#CBE0F0"
+        local fg_dark = "#B4D0E9"
+        local fg_gutter = "#627E97"
+        local border = "#547998"
+
+        require("tokyonight").setup({
+          style = "night",
+          on_colors = function(colors)
+            colors.bg = bg
+            colors.bg_dark = bg_dark
+            colors.bg_float = bg_dark
+            colors.bg_highlight = bg_highlight
+            colors.bg_popup = bg_dark
+            colors.bg_search = bg_search
+            colors.bg_sidebar = bg_dark
+            colors.bg_statusline = bg_dark
+            colors.bg_visual = bg_visual
+            colors.border = border
+            colors.fg = fg
+            colors.fg_dark = fg_dark
+            colors.fg_float = fg
+            colors.fg_gutter = fg_gutter
+            colors.fg_sidebar = fg_dark
+          end,
+        })
+        -- load the colorscheme here
+        vim.cmd([[colorscheme tokyonight]])
       end,
     },
 
@@ -102,17 +141,72 @@ require("lazy").setup({
     {
       'nvim-lualine/lualine.nvim',
       dependencies = { 
-        'nvim-tree/nvim-web-devicons'
+        'nvim-tree/nvim-web-devicons',
+        enabled = vim.g.have_nerd_font
       },
       config = function()
-        require('lualine').setup {
-          options = {
-            icons_enabled = true,
-            theme = 'gruvbox',
-            component_separators = { left = '', right = ''},
-            section_separators = { left = '', right = ''},
-          }
+        local lualine = require("lualine")
+        local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+        local colors = {
+          blue = "#65D1FF",
+          green = "#3EFFDC",
+          violet = "#FF61EF",
+          yellow = "#FFDA7B",
+          red = "#FF4A4A",
+          fg = "#c3ccdc",
+          bg = "#112638",
+          inactive_bg = "#2c3043",
         }
+        local my_lualine_theme = {
+          normal = {
+            a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
+            b = { bg = colors.bg, fg = colors.fg },
+            c = { bg = colors.bg, fg = colors.fg },
+          },
+          insert = {
+            a = { bg = colors.green, fg = colors.bg, gui = "bold" },
+            b = { bg = colors.bg, fg = colors.fg },
+            c = { bg = colors.bg, fg = colors.fg },
+          },
+          visual = {
+            a = { bg = colors.violet, fg = colors.bg, gui = "bold" },
+            b = { bg = colors.bg, fg = colors.fg },
+            c = { bg = colors.bg, fg = colors.fg },
+          },
+          command = {
+            a = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
+            b = { bg = colors.bg, fg = colors.fg },
+            c = { bg = colors.bg, fg = colors.fg },
+          },
+          replace = {
+            a = { bg = colors.red, fg = colors.bg, gui = "bold" },
+            b = { bg = colors.bg, fg = colors.fg },
+            c = { bg = colors.bg, fg = colors.fg },
+          },
+          inactive = {
+            a = { bg = colors.inactive_bg, fg = colors.semilightgray, gui = "bold" },
+            b = { bg = colors.inactive_bg, fg = colors.semilightgray },
+            c = { bg = colors.inactive_bg, fg = colors.semilightgray },
+          },
+        }
+        -- configure lualine with modified theme
+        lualine.setup({
+          options = {
+            theme = my_lualine_theme,
+          },
+          sections = {
+            lualine_x = {
+              {
+                lazy_status.updates,
+                cond = lazy_status.has_updates,
+                color = { fg = "#ff9e64" },
+              },
+              { "encoding" },
+              { "fileformat" },
+              { "filetype" },
+            },
+          },
+        })
       end,
     },
 
@@ -122,7 +216,8 @@ require("lazy").setup({
       build = ":TSUpdate",
       config = function()
         require("nvim-treesitter.configs").setup({
-          ensure_installed = "all", -- Install all supported parsers
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+          auto_install = true,
           highlight = { enable = true },
           indent = { enable = true },
           incremental_selection = {
@@ -137,6 +232,93 @@ require("lazy").setup({
         })
       end,
     },
+
+    { -- Fuzzy Finder (files, lsp, etc)
+      'nvim-telescope/telescope.nvim',
+      event = 'VimEnter',
+      branch = '0.1.x',
+      dependencies = {
+        'nvim-lua/plenary.nvim',
+        { -- If encountering errors, see telescope-fzf-native README for installation instructions
+          'nvim-telescope/telescope-fzf-native.nvim',
+
+          -- `build` is used to run some command when the plugin is installed/updated.
+          -- This is only run then, not every time Neovim starts up.
+          build = 'make',
+
+          -- `cond` is a condition used to determine whether this plugin should be
+          -- installed and loaded.
+          cond = function()
+            return vim.fn.executable 'make' == 1
+          end,
+        },
+        { 'nvim-telescope/telescope-ui-select.nvim' },
+
+        -- Useful for getting pretty icons, but requires a Nerd Font.
+        { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+      },
+      config = function()
+        require('telescope').setup {
+          -- You can put your default mappings / updates / etc. in here
+          --  All the info you're looking for is in `:help telescope.setup()`
+          --
+          defaults = {
+            mappings = {
+              i = { 
+                ["<C-k>"] = require("telescope.actions").move_selection_previous, -- move to prev result
+                ["<C-j>"] = require("telescope.actions").move_selection_next, -- move to next result
+              },
+            },
+          },
+          extensions = {
+            ['ui-select'] = {
+              require('telescope.themes').get_dropdown(),
+            },
+          },
+        }
+
+        -- Enable Telescope extensions if they are installed
+        pcall(require('telescope').load_extension, 'fzf')
+        pcall(require('telescope').load_extension, 'ui-select')
+
+        -- See `:help telescope.builtin`
+        local builtin = require 'telescope.builtin'
+        vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+        vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+        vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+        vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+        vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+        vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+        vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+        vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+        vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+        vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+
+        -- Slightly advanced example of overriding default behavior and theme
+        vim.keymap.set('n', '<leader>/', function()
+          -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+          builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+            winblend = 10,
+            previewer = false,
+          })
+        end, { desc = '[/] Fuzzily search in current buffer' })
+
+        -- It's also possible to pass additional configuration options.
+        --  See `:help telescope.builtin.live_grep()` for information about particular keys
+        vim.keymap.set('n', '<leader>s/', function()
+          builtin.live_grep {
+            grep_open_files = true,
+            prompt_title = 'Live Grep in Open Files',
+          }
+        end, { desc = '[S]earch [/] in Open Files' })
+
+        -- Shortcut for searching your Neovim configuration files
+        vim.keymap.set('n', '<leader>sn', function()
+          builtin.find_files { cwd = vim.fn.stdpath 'config' }
+        end, { desc = '[S]earch [N]eovim files' })
+      end,
+    },
+
     --------------------------------------------------------------------------------
   },
   -- Configure any other settings here. See the documentation for more details.
