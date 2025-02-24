e5e50ee1d190ddf8f117b13d802f2ca2830f7f43
Dixit Bhuva <mail@dixit.dev>
2025-02-24 17:16:06 +0530

    random bullshit

--- init.lua
+++ init.lua
@@ -1,20 +1,26 @@
------ Disable perl ruby and python support -----
+----- disable perl ruby and python support -----
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_python3_provider = 0
 
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
+----- bootstrap lazy.nvim -----
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
 
--- BASIC OPTIONS --
+---- basic options ----
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.opt.updatetime = 50
@@ -43,10 +49,37 @@ vim.opt.backspace = "indent,eol,start"
 vim.opt.clipboard:append("unnamedplus")
 vim.opt.splitright = true
 vim.opt.splitbelow = true
+vim.opt.virtualedit = "block"
 vim.cmd("syntax on")
 
+----- highlights when yanking -----
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
 
------ BASIC KEYMAPS -----
+----- set specific indentation for different file types -----
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "python", "java", "c", "cpp", "php", "yaml", "json", "lua", "sh" },  -- 4 spaces languages
+  callback = function()
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.tabstop = 4
+    vim.opt_local.softtabstop = 4
+  end,
+})
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "javascript", "typescript", "javascriptreact", "typescriptreact", "html", "css", "bash", "toml", "xml", "ini" },  -- 2 spaces languages
+  callback = function()
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
+  end,
+})
+
+----- basic keymaps -----
 vim.keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 vim.keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
 vim.keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
@@ -64,4 +97,185 @@ vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", { desc = "Navigate vim panes to j/
 vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", { desc = "Navigate vim panes to h/left" })
 vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", { desc = "Navigate vim panes to l/right" })
 
-require("config.lazy")
+require("lazy").setup({
+  spec = {
+    ---------------------------------
+    ----- add your plugins here -----
+    ---------------------------------
+    ----- colorscheme -----
+    {
+      "folke/tokyonight.nvim",
+      priority = 1000, -- make sure to load this before all other start plugins
+      config = function()
+        local bg = "#011628"
+        local bg_dark = "#011423"
+        local bg_highlight = "#143652"
+        local bg_search = "#0A64AC"
+        local bg_visual = "#275378"
+        local fg = "#CBE0F0"
+        local fg_dark = "#B4D0E9"
+        local fg_gutter = "#627E97"
+        local border = "#547998"
+
+        require("tokyonight").setup({
+          style = "night",
+          on_colors = function(colors)
+            colors.bg = bg
+            colors.bg_dark = bg_dark
+            colors.bg_float = bg_dark
+            colors.bg_highlight = bg_highlight
+            colors.bg_popup = bg_dark
+            colors.bg_search = bg_search
+            colors.bg_sidebar = bg_dark
+            colors.bg_statusline = bg_dark
+            colors.bg_visual = bg_visual
+            colors.border = border
+            colors.fg = fg
+            colors.fg_dark = fg_dark
+            colors.fg_float = fg
+            colors.fg_gutter = fg_gutter
+            colors.fg_sidebar = fg_dark
+          end,
+        })
+        -- load the colorscheme here
+        vim.cmd([[colorscheme tokyonight]])
+      end,
+    },
+    ----- web dev icons -----
+    { 
+      'nvim-tree/nvim-web-devicons', 
+      enabled = vim.g.have_nerd_font
+    },
+    ----- lua line -----
+    {
+      'nvim-lualine/lualine.nvim',
+      dependencies = { 
+        'nvim-tree/nvim-web-devicons',
+        enabled = vim.g.have_nerd_font
+      },
+      config = function()
+        local lualine = require("lualine")
+        local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+        local colors = {
+          blue = "#65D1FF",
+          green = "#3EFFDC",
+          violet = "#FF61EF",
+          yellow = "#FFDA7B",
+          red = "#FF4A4A",
+          fg = "#c3ccdc",
+          bg = "#112638",
+          inactive_bg = "#2c3043",
+        }
+        local my_lualine_theme = {
+          normal = {
+            a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
+            b = { bg = colors.bg, fg = colors.fg },
+            c = { bg = colors.bg, fg = colors.fg },
+          },
+          insert = {
+            a = { bg = colors.green, fg = colors.bg, gui = "bold" },
+            b = { bg = colors.bg, fg = colors.fg },
+            c = { bg = colors.bg, fg = colors.fg },
+          },
+          visual = {
+            a = { bg = colors.violet, fg = colors.bg, gui = "bold" },
+            b = { bg = colors.bg, fg = colors.fg },
+            c = { bg = colors.bg, fg = colors.fg },
+          },
+          command = {
+            a = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
+            b = { bg = colors.bg, fg = colors.fg },
+            c = { bg = colors.bg, fg = colors.fg },
+          },
+          replace = {
+            a = { bg = colors.red, fg = colors.bg, gui = "bold" },
+            b = { bg = colors.bg, fg = colors.fg },
+            c = { bg = colors.bg, fg = colors.fg },
+          },
+          inactive = {
+            a = { bg = colors.inactive_bg, fg = colors.semilightgray, gui = "bold" },
+            b = { bg = colors.inactive_bg, fg = colors.semilightgray },
+            c = { bg = colors.inactive_bg, fg = colors.semilightgray },
+          },
+        }
+        -- configure lualine with modified theme
+        lualine.setup({
+          options = {
+            theme = my_lualine_theme,
+          },
+          sections = {
+            lualine_x = {
+              {
+                lazy_status.updates,
+                cond = lazy_status.has_updates,
+                color = { fg = "#ff9e64" },
+              },
+              { "encoding" },
+              { "fileformat" },
+              { "filetype" },
+            },
+          },
+        })
+      end,
+    },
+    ----- treesitter -----
+    {
+      "nvim-treesitter/nvim-treesitter",
+      config = function()
+        require'nvim-treesitter.configs'.setup ({
+           ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "html", "css", "javascript", "typescript" },
+           auto_install = true,
+           highlight = {
+             enable = true,
+           },
+           incremental_selection = {
+             enable = true,
+             keymaps = {
+               init_selection = "<C-space>",
+               node_incremental = "<C-space>",
+               scope_incremental = false,
+               node_decremental = "<bs>",
+             },
+           },
+        })
+      end,
+    },
+    {
+      "neovim/nvim-lspconfig",
+      config = function()
+        require('lspconfig').clangd.setup({})
+        require('lspconfig').lua_ls.setup({})
+      end,
+    },
+    {
+      "williamboman/mason.nvim",
+      config = function()
+        require("mason").setup({})
+      end,
+    },
+    ---------------------------------
+    ---- plugins end here -----------
+    ---------------------------------
+  },
+  install = { colorscheme = { "tokyonight" } },
+  checker = { enabled = true },
+  ui = {
+    -- If you are using a Nerd Font: set icons to an empty table which will use the
+    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
+    },
+  }
+})
