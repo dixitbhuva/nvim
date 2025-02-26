66446d041b9ef13cac6a061b16864743165a8d2d
Dixit Bhuva <mail@dixit.dev>
2025-02-26 13:06:50 +0530

    simple config done

---
+++ init.lua
@@ -0,0 +1,311 @@
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_python3_provider = 0
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+--basic options
+vim.opt.updatetime = 50
+vim.opt.guicursor = ""
+vim.opt.wrap = false
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 8
+vim.opt.cursorline = true
+vim.opt.signcolumn = "yes"
+vim.opt.colorcolumn = "80"
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
+vim.opt.backspace = "indent,eol,start"
+vim.opt.clipboard:append("unnamedplus")
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.virtualedit = "block"
+vim.cmd("syntax on")
+
+-- basic keymaps
+vim.keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+vim.keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
+vim.keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
+--vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+--vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+--vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
+--vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
+vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
+vim.keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
+vim.keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
+vim.keymap.set("n", "<C-k>", ":wincmd k<CR>", { desc = "Navigate vim panes to k/up" })
+vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", { desc = "Navigate vim panes to j/down" })
+vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", { desc = "Navigate vim panes to h/left" })
+vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", { desc = "Navigate vim panes to l/right" })
+
+-- highlight when yanking (copying) text
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- set specific indentation for different file types
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "python", "java", "c", "cpp", "php", "yaml", "json", "lua", "sh" }, -- 4 spaces languages
+  callback = function()
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.tabstop = 4
+    vim.opt_local.softtabstop = 4
+  end,
+})
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "javascript", "typescript", "javascriptreact", "typescriptreact", "html", "css", "bash", "toml", "xml", "ini" }, -- 2 spaces languages
+  callback = function()
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
+  end,
+})
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    -- add your plugins here
+
+    {
+      "folke/tokyonight.nvim",
+      priority = 1000, -- make sure to load this before all other start plugins
+      config = function()
+        local bg = "#011628"
+        local bg_dark = "#011423"
+        local bg_highlight = "#143652"
+        local bg_search = "#0A64AC"
+        local bg_visual = "#275378"
+        local fg = "#CBE0F0"
+        local fg_dark = "#B4D0E9"
+        local fg_gutter = "#627E97"
+        local border = "#547998"
+        require("tokyonight").setup({
+          style = "night",
+          on_colors = function(colors)
+            colors.bg = bg
+            colors.bg_dark = bg_dark
+            colors.bg_float = bg_dark
+            colors.bg_highlight = bg_highlight
+            colors.bg_popup = bg_dark
+            colors.bg_search = bg_search
+            colors.bg_sidebar = bg_dark
+            colors.bg_statusline = bg_dark
+            colors.bg_visual = bg_visual
+            colors.border = border
+            colors.fg = fg
+            colors.fg_dark = fg_dark
+            colors.fg_float = fg
+            colors.fg_gutter = fg_gutter
+            colors.fg_sidebar = fg_dark
+          end,
+        })
+        -- load the colorscheme here
+        vim.cmd([[colorscheme tokyonight]])
+      end,
+    },
+
+    {
+      "nvim-lualine/lualine.nvim",
+      dependencies = { 
+        "nvim-tree/nvim-web-devicons",
+        enabled = vim.g.have_nerd_font
+      },
+      config = function()
+        local lualine = require("lualine")
+        local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+        local colors = {
+          blue = "#65D1FF",
+          green = "#3EFFDC",
+          violet = "#FF61EF",
+          yellow = "#FFDA7B",
+          red = "#FF4A4A",
+          fg = "#c3ccdc",
+          bg = "#112638",
+          inactive_bg = "#2c3043",
+        }
+        local my_lualine_theme = {
+          normal = {
+            a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
+            b = { bg = colors.bg, fg = colors.fg },
+            c = { bg = colors.bg, fg = colors.fg },
+          },
+          insert = {
+            a = { bg = colors.green, fg = colors.bg, gui = "bold" },
+            b = { bg = colors.bg, fg = colors.fg },
+            c = { bg = colors.bg, fg = colors.fg },
+          },
+          visual = {
+            a = { bg = colors.violet, fg = colors.bg, gui = "bold" },
+            b = { bg = colors.bg, fg = colors.fg },
+            c = { bg = colors.bg, fg = colors.fg },
+          },
+          command = {
+            a = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
+            b = { bg = colors.bg, fg = colors.fg },
+            c = { bg = colors.bg, fg = colors.fg },
+          },
+          replace = {
+            a = { bg = colors.red, fg = colors.bg, gui = "bold" },
+            b = { bg = colors.bg, fg = colors.fg },
+            c = { bg = colors.bg, fg = colors.fg },
+          },
+          inactive = {
+            a = { bg = colors.inactive_bg, fg = colors.semilightgray, gui = "bold" },
+            b = { bg = colors.inactive_bg, fg = colors.semilightgray },
+            c = { bg = colors.inactive_bg, fg = colors.semilightgray },
+          },
+        }
+        -- configure lualine with modified theme
+        lualine.setup({
+          options = {
+            theme = my_lualine_theme,
+          },
+          sections = {
+            lualine_x = {
+              {
+                lazy_status.updates,
+                cond = lazy_status.has_updates,
+                color = { fg = "#ff9e64" },
+              },
+              { "encoding" },
+              { "fileformat" },
+              { "filetype" },
+            },
+          },
+        })
+      end,
+    },
+
+    {
+      "nvim-treesitter/nvim-treesitter",
+      config = function()
+        require'nvim-treesitter.configs'.setup ({
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "html", "css", "javascript", "typescript" },
+          auto_install = true,
+          highlight = {
+            enable = true,
+          },
+          incremental_selection = {
+            enable = true,
+            keymaps = {
+              init_selection = "<C-space>",
+              node_incremental = "<C-space>",
+              scope_incremental = false,
+              node_decremental = "<bs>",
+            },
+          },
+        })
+      end,
+    },
+
+    {
+      'nvim-telescope/telescope.nvim',
+      event = 'VimEnter',
+      branch = '0.1.x',
+      dependencies = {
+        'nvim-lua/plenary.nvim',
+        {
+          'nvim-telescope/telescope-fzf-native.nvim',
+          build = 'make',
+          cond = function()
+            return vim.fn.executable 'make' == 1
+          end,
+        },
+        { 'nvim-telescope/telescope-ui-select.nvim' },
+        { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+      },
+      config = function()
+        require('telescope').setup ({
+          defaults = {
+            path_display = { "smart" },
+            mappings = {
+              i = { 
+                ["<C-k>"] = require("telescope.actions").move_selection_previous, -- move to prev result
+                ["<C-j>"] = require("telescope.actions").move_selection_next, -- move to next result
+              },
+            },
+          },
+          extensions = {
+            ['ui-select'] = {
+              require('telescope.themes').get_dropdown(),
+            },
+          },
+        })
+
+        pcall(require('telescope').load_extension, 'fzf')
+        pcall(require('telescope').load_extension, 'ui-select')
+
+        local builtin = require 'telescope.builtin'
+        vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+        vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+        vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+        vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+        vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+        vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+        vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+        vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+        vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+        vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+
+        vim.keymap.set('n', '<leader>/', function()
+          builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+            winblend = 10,
+            previewer = false,
+          })
+        end, { desc = '[/] Fuzzily search in current buffer' })
+
+        vim.keymap.set('n', '<leader>s/', function()
+          builtin.live_grep {
+            grep_open_files = true,
+            prompt_title = 'Live Grep in Open Files',
+          }
+        end, { desc = '[S]earch [/] in Open Files' })
+
+        vim.keymap.set('n', '<leader>sn', function()
+          builtin.find_files { cwd = vim.fn.stdpath 'config' }
+        end, { desc = '[S]earch [N]eovim files' })
+      end,
+    },
+
+    -- plugins end
+  },
+  install = { colorscheme = { "tokyonight" } },
+  checker = { enabled = true },
+})
